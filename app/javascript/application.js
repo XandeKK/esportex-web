1f4a5c98e462f9855ad8cbf6f88b41f6e50113aa
Alexandre <alexandre.dosa@gmail.com>
2022-07-09 16:06:10 -0300

    Update application files and add components partial

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	hideFlash();
+});
+
+function hideFlash() {
+    setInterval(function() {
+     	flash.classList.add('fade');
+    }, 5000);
+}
