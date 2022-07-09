a8d28f22bcfdd5aaa384b696a19d023bcf16c982
Alexandre <alexandre.dosa@gmail.com>
2022-07-09 15:35:47 -0300

    Add model user

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_one_attached :avatar
+  has_many :games, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 3..20 }
+  validates :username, presence: true, uniqueness: true, length: { in: 3..20 } # Colocar regex
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+    message: "only allows letters, number, '-', '+' and '_'", :multiline => true }
+  validates :bio, length: { maximum: 300 }
+  
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.with(user: self).forgot_password.deliver_now
+  end
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+
+end
