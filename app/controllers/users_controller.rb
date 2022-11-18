ee6bcb46771e356958fea36746230913aa832c7a
xandekk <alexandre.dosa@gmail.com>
2022-11-18 19:42:14 -0300

    Add view clearance and controller users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+	private
+
+  def user_from_params
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+    name = user_params.delete(:name)
+    username = user_params.delete(:username)
+    bio = user_params.delete(:bio)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.password = password
+      user.name = name
+      user.username = username
+      user.bio = bio
+    end
+  end
+end
